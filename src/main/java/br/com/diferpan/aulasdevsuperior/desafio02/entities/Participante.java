c701e21a435ad33a06d11d27f73e2722a5f1b733
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-10 11:58:02 -0300

    criação da classe base Participante.java

--- src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Participante.java
+++ src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Participante.java
@@ -1,6 +1,20 @@
 package br.com.diferpan.aulasdevsuperior.desafio02.entities;
-
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name="tb_participante")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Participante {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
 
 }
