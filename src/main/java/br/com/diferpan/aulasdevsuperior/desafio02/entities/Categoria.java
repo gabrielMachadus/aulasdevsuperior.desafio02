b9c8021c90b73e082ca9070b49d5b2484c5510c0
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-10 15:23:40 -0300

    inclusão dos campos e notations para Categoria.java

--- src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Categoria.java
+++ src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Categoria.java
@@ -1,4 +1,21 @@
 package br.com.diferpan.aulasdevsuperior.desafio02.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tb_categoria")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String descricao;
 }
