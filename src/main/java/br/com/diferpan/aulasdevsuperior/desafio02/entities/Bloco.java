05f78944d74b570bc1c0a8dafa27f054ef371b44
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-10 15:26:31 -0300

    inclusão dos campos e notations para Bloco.java

--- src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Bloco.java
+++ src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Bloco.java
@@ -1,4 +1,26 @@
 package br.com.diferpan.aulasdevsuperior.desafio02.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+
+@Entity
+@Table(name="tb_bloco")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bloco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
 }
