4494f9bde23f30446b8e38f230ea5f601c94cf31
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-10 15:24:00 -0300

    inclusão dos campos e notations para Atividade.java

--- src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Atividade.java
+++ src/main/java/br/com/diferpan/aulasdevsuperior/desafio02/entities/Atividade.java
@@ -1,4 +1,22 @@
 package br.com.diferpan.aulasdevsuperior.desafio02.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tb_atividade")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String descricao;
+    private String nome;
+    private Double preco;
 }
